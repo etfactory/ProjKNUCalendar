63ca765019df73a856d2eec8d4471911937abda5
Euntaek Oh <etfactory@icloud.com>
2022-11-25 22:47:45 +0900

    달력 패널 테스트용 업데이트

--- src/project/knucalendar/mainPanel.java
+++ src/project/knucalendar/mainPanel.java
@@ -11,14 +11,16 @@ public class mainPanel extends JFrame implements ActionListener {
     JFrame mainFrame;
     JPanel centrePanel, topPanel, bottomPanel;
     JButton linkedKNU, linkedLMS, linkedSugang;
+    JPanel calendarPanel;
+    JButton weekDaysName[] = new JButton[7];
+    String WEEK_DAY_NAME[] = { "SUN", "MON", "TUE", "WED", "THR", "FRI", "SAT" };
+    JPanel dateButton[][] = new JPanel[6][7];
     ImageIcon icon = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/icon/bear/bearVersion1_1.png")));
     ImageIcon normalBackground = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/background (1280x720)/centre.png")));
     ImageIcon bottomNormal = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/background (1280x720)/bottom.png")));
     ImageIcon KNUPage = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/button/KNUPage.png")));
     ImageIcon LMSPage = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/button/LMSPage.png")));
     ImageIcon SugangPage = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/button/수강신청.png")));
-
-    makingCalendar makingCalendar;
 
     public mainPanel(){
         mainFrame = new JFrame(title);
@@ -53,7 +55,36 @@ public class mainPanel extends JFrame implements ActionListener {
         /*
         여기에 일정, 달력 순 생성
          */
+        calendarPanel = new JPanel();
+        calendarPanel.setLayout(new GridLayout(0,7,2,2));
+        calendarPanel.setSize(600,500);
+        for(int i=0;i<7;i++){
+            weekDaysName[i] =new JButton(WEEK_DAY_NAME[i]);
+            weekDaysName[i].setBorderPainted(false);
+            weekDaysName[i].setContentAreaFilled(false);
+            weekDaysName[i].setForeground(Color.WHITE);
+            if(i==0)
+                weekDaysName[i].setBackground(new Color(200,50,50));
+            else if (i==6)
+                weekDaysName[i].setBackground(new Color(50,100,200));
+            else
+                weekDaysName[i].setBackground(new Color(150,150,150));
+            weekDaysName[i].setOpaque(true);
+            weekDaysName[i].setFocusPainted(false);
+            calendarPanel.add(weekDaysName[i]);
+        }
+        for(int i=0;i<6;i++){
+            for(int j=0;j<7;j++){
+                dateButton[i][j]=new JPanel();
+                dateButton[i][j].setLayout(new BorderLayout());
+                dateButton[i][j].setBackground(Color.WHITE);
+                dateButton[i][j].setOpaque(true);
+                calendarPanel.add(dateButton[i][j]);
+            }
+        }
+
         centrePanel.setLayout(new FlowLayout());
+        centrePanel.add(calendarPanel);
 
         bottomPanel = new JPanel(){
             public void paintComponent(Graphics g){
