d9823036cf13f388591dd2a2ec35cf22dac71af7
Euntaek Oh <etfactory@icloud.com>
2022-11-27 02:37:19 +0900

    SQLite로 변경

--- src/project/knucalendar/connectDB.java
+++ src/project/knucalendar/connectDB.java
@@ -5,28 +5,20 @@ import java.awt.*;
 import java.sql.*;
 
 public class connectDB {
-
-    ImageIcon calendarForUser = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/gui/diaryIcon/user.png")));
     public connectDB(){
         Connection conn;
-        String url = "jdbc:mysql://localhost:3306/diarydata?serverTimezon=UTC";
-        String id = "root";
-        String pw = "pw123";
-
+        String url = "jdbc:sqlite:database:db";
         Statement stmt;
         ResultSet result;
 
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(url,id,pw);
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(url);
             System.out.println("DB 연결 완료");
 
-            stmt = conn.createStatement();
-
-            stmt.executeUpdate("insert into diarydata values(2022 ,12 , 25, 'Christmas', NULL, NULL, calendarForUser)");
-        } catch(ClassNotFoundException e){
-            System.out.println();
-        } catch(SQLException e){
+            stmt=conn.createStatement();
+            result=stmt.executeQuery("select * from diarydata;");
+        } catch(Exception e) {
             System.out.println();
         }
     }
