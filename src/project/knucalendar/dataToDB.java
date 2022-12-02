0ccc57a4766dad9adeeb70b45595a149a878ba93
Euntaek Oh <etfactory@icloud.com>
2022-12-02 19:11:27 +0900

    1202 변경사항 - 짜증나서 못해먹겠다

--- src/project/knucalendar/dataToDB.java
+++ src/project/knucalendar/dataToDB.java
@@ -4,6 +4,7 @@ import java.sql.*;
 
 public class dataToDB {
     Connection conn = null;
+    PreparedStatement pstmt;
     String url = "jdbc:sqlite:database:db";
     int getStartDate, getEndDate, getDday;
     public int getDday(int getStartDate, int getEndDate){
@@ -20,44 +21,50 @@ public class dataToDB {
 
         getDday = getDday(getStartDate, getEndDate);
 
+        System.out.println(getStartDate+" "+getEndDate+" "+nameData+" "+kindData+" "+whereData+" "+stringData+" "+getStartYear);
+
         try {
             Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(url);
 
-            for (int i=0;i<getDday;i++){
-                String insertSQL = "insert into diarydata values(?,?,?,?,?,?,?,?)";
-                PreparedStatement pstmt=conn.prepareStatement(insertSQL);
+            String insertSQL = "insert into diarydata values(?,?,?,?,?,?,?,?)";
+            pstmt = conn.prepareStatement(insertSQL);
+            pstmt.setInt(1,getStartYear);
+            pstmt.setInt(2,getStartMonth);
+            pstmt.setInt(3,getStartDayofMonth);
+            pstmt.setString(4,nameData);
 
-                pstmt.setInt(1,getStartYear);
-                pstmt.setInt(2,getStartMonth);
-                pstmt.setInt(3,getStartDayofMonth);
-                pstmt.setString(4,nameData);
-                if (kindData.equals("학사일정"))
-                    pstmt.setString(5,"university");
-                else if (kindData.equals("공휴일"))
-                    pstmt.setString(5,"legalholiday");
-                else if (kindData.equals("시험"))
-                    pstmt.setString(5,"test");
-                else if (kindData.equals("과제"))
-                    pstmt.setString(5,"homework");
-                else if (kindData.equals("개인일정"))
-                    pstmt.setString(5,"user");
+            if (kindData.equals("학사일정"))
+                pstmt.setString(5,"university");
+            else if (kindData.equals("공휴일"))
+                pstmt.setString(5,"legalholiday");
+            else if (kindData.equals("시험"))
+                pstmt.setString(5,"test");
+            else if (kindData.equals("과제"))
+                pstmt.setString(5,"homework");
+            else if (kindData.equals("개인일정"))
+                pstmt.setString(5,"user");
+            if (stringData.equals(""))
+                pstmt.setString(6,null);
+            else
                 pstmt.setString(6,stringData);
+            if (whereData.equals(""))
+                pstmt.setString(7,null);
+            else
                 pstmt.setString(7,whereData);
-                if (kindData.equals("학사일정"))
-                    pstmt.setString(8,"university.png");
-                else if (kindData.equals("공휴일"))
-                    pstmt.setString(8,"user.png");
-                else if (kindData.equals("시험"))
-                    pstmt.setString(8,"test.png");
-                else if (kindData.equals("과제"))
-                    pstmt.setString(8,"homework.png");
-                else if (kindData.equals("개인일정"))
-                    pstmt.setString(8,"user.png");
 
-                int cnt = pstmt.executeUpdate();
-                System.out.println("데이터가 추가되었습니다.");
-            }
+            if (kindData.equals("학사일정"))
+                pstmt.setString(8,"university.png");
+            else if (kindData.equals("공휴일"))
+                pstmt.setString(8,"user.png");
+            else if (kindData.equals("시험"))
+                pstmt.setString(8,"test.png");
+            else if (kindData.equals("과제"))
+                pstmt.setString(8,"homework.png");
+            else if (kindData.equals("개인일정"))
+                pstmt.setString(8,"user.png");
+
+            pstmt.executeUpdate();
         } catch(Exception e) {
             System.out.println();
         }
