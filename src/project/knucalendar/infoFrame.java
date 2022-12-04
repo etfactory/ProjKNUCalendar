a5374fc6173c7cb4300e30a4dbe3496b27ce4634
Euntaek Oh <etfactory@icloud.com>
2022-12-04 11:29:35 +0900

    프로그램 정보 패널 업데이트

--- src/project/knucalendar/infoFrame.java
+++ src/project/knucalendar/infoFrame.java
@@ -5,16 +5,79 @@ import java.awt.*;
 
 public class infoFrame {
     JFrame infoFrame;
+    JPanel infoPanel;
 
     ImageIcon icon = new ImageIcon(Toolkit.getDefaultToolkit().getImage(getClass().getResource("img/icon/bear/bearVersion1_1.png")));
 
     public infoFrame(){
         infoFrame = new JFrame();
-        infoFrame.setSize(500,400);
+        infoFrame.setSize(600,300);
         infoFrame.setLayout(new BorderLayout());
         infoFrame.setLocationRelativeTo(null);
         infoFrame.setResizable(false);
         infoFrame.setIconImage(icon.getImage());
         infoFrame.setVisible(true);
+
+        infoPanel = new JPanel();
+        infoPanel.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+
+        JPanel leftPanel = new JPanel();
+        leftPanel.setLayout(new GridLayout(8,1,10,10));
+
+        JLabel versionTitle = new JLabel("VERSION");
+        versionTitle.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel addressTitle = new JLabel("UNIVERSITY");
+        addressTitle.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel firstdeveloperName = new JLabel("DEVELOPER NAME");
+        firstdeveloperName.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel firstdeveloperGIT = new JLabel("DEVELOPER GIT");
+        firstdeveloperGIT.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel secdeveloperName = new JLabel("DEVELOPER NAME");
+        secdeveloperName.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel secdeveloperGIT = new JLabel("DEVELOPER GIT");
+        secdeveloperGIT.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        JLabel blank = new JLabel(" ");
+        blank.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+
+        leftPanel.add(versionTitle);
+        leftPanel.add(addressTitle);
+        leftPanel.add(firstdeveloperName);
+        leftPanel.add(firstdeveloperGIT);
+        leftPanel.add(secdeveloperName);
+        leftPanel.add(secdeveloperGIT);
+
+        JPanel rightPanel = new JPanel();
+        rightPanel.setLayout(new GridLayout(8,1,10,10));
+
+        JLabel version = new JLabel("0.0.1");
+        version.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+        JLabel address = new JLabel("Kongju National University");
+        address.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+        JLabel firstDeveloperName = new JLabel("Euntaek Oh");
+        firstDeveloperName.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+        JLabel firstDeveloperGit = new JLabel("@etfactory");
+        firstDeveloperGit.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+        JLabel secondDeveloperName = new JLabel("Jeong-woo Choi");
+        secondDeveloperName.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+        JLabel secondDeveloperGit = new JLabel("@ygftcu");
+        secondDeveloperGit.setFont(new Font("나눔바른고딕",Font.PLAIN,16));
+
+        rightPanel.add(version);
+        rightPanel.add(address);
+        rightPanel.add(firstDeveloperName);
+        rightPanel.add(firstDeveloperGit);
+        rightPanel.add(secondDeveloperName);
+        rightPanel.add(secondDeveloperGit);
+
+        infoPanel.add(leftPanel);
+        infoPanel.add(rightPanel);
+
+        JPanel bottomPanel = new JPanel();
+        JLabel bottomText = new JLabel("KNU Calendar Program");
+        bottomText.setFont(new Font("나눔바른고딕",Font.BOLD,16));
+        bottomPanel.add(bottomText);
+
+        infoFrame.add(infoPanel,BorderLayout.CENTER);
+        infoFrame.add(bottomPanel,BorderLayout.SOUTH);
     }
 }
